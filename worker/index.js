7a279693473f20797e6cbd38c8111e3acbbe3c78
Underman, Thomas Richard <undermant12@yahoo.com>
2019-12-11 20:23:25 -0500

    core functionality

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,6 @@
 var CronJob = require('cron').CronJob;
+
 const fetchGithub = require('./tasks/fetch-github')
 
-
-new CronJob(' * * * * *', fetchGithub, null, true, 'America/Los_Angeles');
+// fetch github jobs
+new CronJob('* * * * *', fetchGithub, null, true, 'America/Los_Angeles');
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,27 +1,52 @@
-var fetch = require('node-fetch')
-const baseUrl = "https://jobs.github.com/positions.json"
+var fetch = require('node-fetch');
+var redis = require("redis"),
+    client = redis.createClient();
 
+const {promisify} = require('util');
+const setAsync = promisify(client.set).bind(client);
+
+const baseURL = 'https://jobs.github.com/positions.json'
 
 async function fetchGithub() {
 
-    let resultCount = 1,
-        onPage = 0
+    console.log('fetching github')
 
-    const allJobs = []
+    let resultCount = 1, onPage = 0;
+    const allJobs = [];
 
+    // fetch all pages
     while(resultCount > 0) {
-        const res = await fetch(`${baseUrl}?page=${onPage}`);
+        const res = await fetch(`${baseURL}?page=${onPage}`);
         const jobs = await res.json();
-        allJobs.push(...jobs); //spread operator
+        allJobs.push(...jobs);
         resultCount = jobs.length;
         console.log('got', resultCount, 'jobs');
-        onPage++;     
+        onPage++;
     }
 
-    console.log("got", allJobs.length, "jobs")
+    console.log('got', allJobs.length, 'jobs total')
 
+    // filter algo
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+        // algo logic
+        if (
+            jobTitle.includes('senior') ||
+            jobTitle.includes('manager') ||
+            jobTitle.includes('sr.') ||
+            jobTitle.includes('architect')
+        ) {
+            return false
+        } 
+        return true;
+    })
+
+    console.log('filtered down to', jrJobs.length);
+
+    // set in redis
+    const success = await setAsync('github', JSON.stringify(jrJobs));
+
+    console.log({success});
 }
 
-fetchGithub()
-
-module.exports = fetchGithub
+module.exports = fetchGithub;
